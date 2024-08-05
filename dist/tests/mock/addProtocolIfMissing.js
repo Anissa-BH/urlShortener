561af9860c0f628636ce61f281e7d4fe5a102eb9
Anissa Ben Hafsia <abenhafsia@zaion.ai>
2024-08-05 23:52:59 +0200

    url shortener first commit

--- dist/tests/mock/addProtocolIfMissing.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addProtocolIfMissing = void 0;
-exports.addProtocolIfMissing = jest.fn((url) => {
-    return `http://${url}`;
-});
